3c5a1b80bfac85c20bb0661e44b97a0421ca2e60
Melanie Howard <n9736247@qut.edu.au>
2020-05-30 15:36:54 +1000

    Movie class implemented + adding logic

--- src/app/App.java
+++ src/app/App.java
@@ -32,18 +32,9 @@ public class App {
 
             switch (selection) {
                 case 1:
-                    //credential verification
-                    System.out.print("Enter username: ");
-                    String username = "Kate";
-
-                    username = input.nextLine();
-                    System.out.println(username);
-
-                    //staffMenu();
-                    
+                    staffMenu();
                     break;
                 case 2:
-                    //TODO: implement credential verification
                     memberMenu();
                     break;
                 case 0:
@@ -61,47 +52,156 @@ public class App {
         Scanner input = new Scanner(System.in);
         boolean displayMenu = true;
 
-        while(displayMenu) {
-            System.out.println("\n\n");
-            System.out.println("===========Staff Menu===========");
-            System.out.println("1. Add a new movie DVD");
-            System.out.println("2. Remove a movie DVD");
-            System.out.println("3. Register a new Member");
-            System.out.println("4. Find a registered member's phone number");
-            System.out.println("0. Return to main menu");
-            System.out.println("================================");
-            System.out.print("Please make a selection (1-4, or 0 to return to main menu): ");
+        System.out.print("Enter Username: ");
+        String user = input.nextLine();
 
-            int selection = 9; //set garbage int value to force default case on InputMismatchException
+        if (user.equals("staff")) {
+            System.out.print("Enter Password: ");
+            String pass = input.nextLine();
 
-            try {
-                selection = input.nextInt();
-            } catch (InputMismatchException e) {
-                //System.out.println("Invalid selection");
-            }
+            if (pass.equals("today123")) {
 
-            switch (selection) {
-                case 1:
-                    System.out.println("add a new DVD");
-                    break;
-                case 2:
-                    System.out.println("remove a DVD");
-                    break;
-                case 3:
-                    System.out.println("register member");
-                    break;
-                case 4:
-                    System.out.println("find phone no.");
-                    break;
-                case 0:
-                    System.out.println("Returning to Main Menu...");
-                    displayMenu = false;
-                    break;
-                default:
-                    System.out.println("Invalid selection");
-            }// end switch
+                while(displayMenu) {
 
-        }// end while
+                    System.out.println("\n\n");
+                    System.out.println("===========Staff Menu===========");
+                    System.out.println("1. Add a new movie DVD");
+                    System.out.println("2. Remove a movie DVD");
+                    System.out.println("3. Register a new Member");
+                    System.out.println("4. Find a registered member's phone number");
+                    System.out.println("0. Return to main menu");
+                    System.out.println("================================");
+                    System.out.print("Please make a selection (1-4, or 0 to return to main menu): ");
+
+                    int selection = 9; //set garbage int value to force default case on InputMismatchException
+
+                    try {
+                        selection = input.nextInt();
+                        input.nextLine();
+                    } catch (InputMismatchException e) {
+                        //System.out.println("Invalid selection");
+                    }
+
+                    switch (selection) {
+                        case 1:
+                            Movie movie = new Movie();
+
+                            System.out.print("Enter the movie title: ");
+                            String title = input.nextLine();
+                            movie.title = title;
+                            System.out.print("Enter the starring actors(s): ");
+                            String starring = input.nextLine();
+                            movie.starring = starring;
+                            System.out.print("Enter the director(s): ");
+                            String director = input.nextLine();
+                            movie.director = director;
+
+                            System.out.println("Select the genre:");
+                            System.out.println("1. Drama");
+                            System.out.println("2. Adventure");
+                            System.out.println("3. Family");
+                            System.out.println("4. Action");
+                            System.out.println("5. Sci-Fi");
+                            System.out.println("6. Comedy");
+                            System.out.println("7. Thriller");
+                            System.out.println("8. Other");
+                            System.out.print("Make Selection(1-8): ");
+
+                            String genre = input.nextLine();
+                            switch (genre) {
+                                case "1":
+                                    movie.genre = "Drama";
+                                    break;
+                                case "2":
+                                    movie.genre = "Adventure";
+                                    break;
+                                case "3":
+                                    movie.genre = "Family";
+                                    break;
+                                case "4":
+                                    movie.genre = "Action";
+                                    break;
+                                case "5":
+                                    movie.genre = "Sci-Fi";
+                                    break;
+                                case "6":
+                                    movie.genre = "Comedy";
+                                    break;
+                                case "7":
+                                    movie.genre = "Thriller";
+                                    break;
+                                case "8":
+                                    movie.genre = "Other";
+                                    break;
+                                default:
+                                    movie.genre = "Other";
+                            }// end genre switch
+
+                            System.out.println("Select the classification:");
+                            System.out.println("1. General (G)");
+                            System.out.println("2. Parental Guidance (PG)");
+                            System.out.println("3. Mature (M15+)");
+                            System.out.println("4. Mature Accompanied (MA15+)");
+                            System.out.print("Make Selection(1-4): ");
+
+                            String classification = input.nextLine();
+                            switch (classification) {
+                                case "1":
+                                    movie.classification = "General (G)";
+                                    break;
+                                case "2":
+                                    movie.classification = "Parental Guidance (PG)";
+                                    break;
+                                case "3":
+                                    movie.classification = "Mature (M15+)";
+                                    break;
+                                case "4":
+                                    movie.classification = "Mature Accompanied (MA15+)";
+                                    break;
+                                default:
+                                    movie.classification = "Unrated";
+                            }//end classification switch
+
+                            System.out.print("Enter the duration (minutes): ");
+                            String duration = input.nextLine();
+                            movie.duration = Integer.parseInt(duration);
+                            System.out.print("Enter the release date (year): ");
+                            String date = input.nextLine();
+                            movie.date = date;
+                            System.out.print("Enter the number of copies available: ");
+                            int copies = Integer.parseInt(input.nextLine());
+
+                            //add movie(s) to MovieCollection
+
+
+                            break;
+                        case 2:
+                            System.out.println("remove a DVD");
+                            break;
+                        case 3:
+                            System.out.println("register member");
+                            break;
+                        case 4:
+                            System.out.println("find phone no.");
+                            break;
+                        case 0:
+                            System.out.println("Returning to Main Menu...");
+                            displayMenu = false;
+                            break;
+                        default:
+                            System.out.println("Invalid selection");
+                    }// end switch
+
+                }// end while
+                
+            } else {
+                System.out.println("Invalid Password");
+            }//end if password verification
+
+        } else {
+            System.out.println("Invalid Username");
+        }//end if username verification
+            
     }// function staffMenu
 
     public static void memberMenu() {
@@ -124,6 +224,7 @@ public class App {
 
             try {
                 selection = input.nextInt();
+                input.nextLine();
             } catch (InputMismatchException e) {
                 //System.out.println("Invalid selection");
             }
---
+++ src/app/Movie.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Movie {
+  String title;
+  String starring;
+  String director;
+  int duration;
+  String genre;
+  String classification;
+  String date;
+}
